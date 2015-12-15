1a35a7c777d81a54c701315e77bdb3c5bdcaac68
jjacobson <jjacobson@catalystitservices.com>
2015-12-15 15:49:15 -0800

    Commented out test routes

--- routes/index.js
+++ routes/index.js
@@ -14,43 +14,43 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-//Test routes to get data from db
-router.get('/users', function(req, res, next) {
-   User.find(function(err, users) {
-       if (err) {
-           return next(err);
-       }
-       res.json(users);
-   });
-});
-
-router.get('/projects', function(req, res, next) {
-   Project.find(function(err, projects) {
-       if (err) {
-           return next(err);
-       }
-       res.json(projects);
-   });
-});
-
-router.post('/reports', function(req, res, next) {
-    var aReport = new Report(req.body);
-    aReport.save(function(err, aReport) {
-       if (err) {
-           return next(err);
-       }
-       res.json(aReport);
-   });
-});
-
-
-router.get('/reports', function(req, res, next) {
-   Report.find(function(err, reports) {
-       if (err) {
-           return next(err);
-       }
-       res.json(reports);
-   });
-});
+// //Test routes to get data from db
+// router.get('/users', function(req, res, next) {
+//    User.find(function(err, users) {
+//        if (err) {
+//            return next(err);
+//        }
+//        res.json(users);
+//    });
+// });
+//
+// router.get('/projects', function(req, res, next) {
+//    Project.find(function(err, projects) {
+//        if (err) {
+//            return next(err);
+//        }
+//        res.json(projects);
+//    });
+// });
+//
+// router.post('/reports', function(req, res, next) {
+//     var aReport = new Report(req.body);
+//     aReport.save(function(err, aReport) {
+//        if (err) {
+//            return next(err);
+//        }
+//        res.json(aReport);
+//    });
+// });
+//
+//
+// router.get('/reports', function(req, res, next) {
+//    Report.find(function(err, reports) {
+//        if (err) {
+//            return next(err);
+//        }
+//        res.json(reports);
+//    });
+// });
 
 module.exports = router;
