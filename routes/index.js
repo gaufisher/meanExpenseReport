899964e6840f94fdbc1b6ef1393a865148dac930
Derek Delaney <ddelaney@catalysitservices.com>
2015-12-15 13:37:16 -0800

    added router for /line-item-types

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var mongoose = require('mongoose');
+var ExpenseReport = mongoose.model('Report');
+
+// Get all line item types
+router.get('/line-item-types', function(req, res, next) {
+    var lineItemTypes = [{value: 'Mileage'}, {value: 'Per Diem'}, {value: 'Lodging'}, {value: 'Travel'}, {value: 'Meals'}, {value: 'Entertainment'}, {value: 'Parking'}, {value: 'Other'}];
+    res.json(lineItemTypes);
+});
+
 module.exports = router;
