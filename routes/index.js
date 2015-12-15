e9155814312e6a86d67f99da0f08e4c6b1c33861
Derek Delaney <ddelaney@catalysitservices.com>
2015-12-15 13:54:53 -0800

    QA'd Card 38

--- routes/index.js
+++ routes/index.js
@@ -14,33 +14,32 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* Test routes to get data from db
-router.get('/users', function(req, res, next) {
-    User.find(function(err, users) {
-        if (err) {
-            return next(err);
-        }
-        res.json(users);
-    });
-});
-
-router.get('/projects', function(req, res, next) {
-    Project.find(function(err, projects) {
-        if (err) {
-            return next(err);
-        }
-        res.json(projects);
-    });
-});
-
-router.get('/reports', function(req, res, next) {
-    Report.find(function(err, reports) {
-        if (err) {
-            return next(err);
-        }
-        res.json(reports);
-    });
-});
-*/
+////Test routes to get data from db
+//router.get('/users', function(req, res, next) {
+//    User.find(function(err, users) {
+//        if (err) {
+//            return next(err);
+//        }
+//        res.json(users);
+//    });
+//});
+//
+//router.get('/projects', function(req, res, next) {
+//    Project.find(function(err, projects) {
+//        if (err) {
+//            return next(err);
+//        }
+//        res.json(projects);
+//    });
+//});
+//
+//router.get('/reports', function(req, res, next) {
+//    Report.find(function(err, reports) {
+//        if (err) {
+//            return next(err);
+//        }
+//        res.json(reports);
+//    });
+//});
 
 module.exports = router;
