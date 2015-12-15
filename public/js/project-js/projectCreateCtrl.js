31956bde5c8c98eda89b403e495f6956e542afaa
Seng Cha <scha@catalystitservices.com>
2015-12-15 14:03:55 -0800

    Create new project with mean stack

--- public/js/project-js/projectCreateCtrl.js
+++ public/js/project-js/projectCreateCtrl.js
@@ -1,22 +1,21 @@
-'use strict';
 
-angular.module('QuickrBooks').controller('projectCreateCtrl', ['$scope', 'projectFactory', 'userFactory', function($scope, projectFactory, userFactory) {
+angular.module('QuickrBooks').controller('projectCreateCtrl', ['$scope', '$resource', 'projectFactory', function($scope, $resource, projectFactory) {
     $scope.newProject = {};
-    userFactory.getCurrentUser().then(
+  /*  userFactory.getCurrentUser().then(
         function(success) {
             $scope.newProject.approver = success.data;
         }
-    );
+    );*/
 
     /* Clears the project save message */
-    $scope.clearResult = function() {
+    $scope.clearResult = function () {
         if ($scope.result !== undefined || $scope.result !== "") {
             $scope.result = "";
         }
     }
 
     /* Shows the save button only if project name is valid. */
-    $scope.textInput = function() {
+    $scope.textInput = function () {
         var namePattern = new RegExp("^[a-zA-Z0-9\-_ ]{1,25}$");
 
         if($scope.projectName !== undefined) {
@@ -27,16 +26,18 @@ angular.module('QuickrBooks').controller('projectCreateCtrl', ['$scope', 'projec
             }
         }
     }
+    var Project = $resource('/project');
 
-    /* Saves the new project to the database, displays a save message,
-       and resets the create new project text box. */
-    $scope.saveProject = function() {
-        $scope.newProject.name = $scope.projectName;
-
-        projectFactory.create($scope.newProject);
-        $scope.showButton = false;
-        $scope.result = "Project " + $scope.projectName + " saved.";
-        $scope.projectName = "";
+    $scope.saveProject = function () {
+        var project = new Project();
+        project.name = $scope.projectName;
+        project.$save(function (result) {
+            $scope.newProject = result;
+            $scope.showButton = false;
+            $scope.result = "Project " + $scope.projectName + " saved.";
+            $scope.projectName = "";
+        });
+        
     }
 
 }]);
