bb8a536d14b086aab13836873d0dcfa7ec251081
Dan Sloane <dsloane@catalystdevworks.com>
2015-12-16 12:56:47 -0800

    faked username for testing

--- public/js/controllers/userCtrl.js
+++ public/js/controllers/userCtrl.js
@@ -1,10 +1,16 @@
 
 app.controller('userCtrl', ['$scope', 'userFactory', 
    function($scope, userFactory){
+	    $scope.user = {};
 		$scope.setUsername = function(){
-			userFactory.getCurrentUser().then(function(success){
+			/*userFactory.getCurrentUser().then(function(success){
 				$scope.userName = success.data.name;
-			});
+			});*/
+			$scope.userName = "dsloane";
+			$scope.user = {
+							"_id": "56707a9e2c29fc36bf61955f",
+							"name": "dsloane"
+						};
 		};
 	}                               
 ]);
