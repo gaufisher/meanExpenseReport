b152602df93895b90ee4fa6dc607d892d835d45d
jjacobson <jjacobson@catalystitservices.com>
2015-12-17 15:26:45 -0800

    Uncommented user now that they are linked properly

--- routes/index.js
+++ routes/index.js
@@ -77,57 +77,24 @@ router.get('/expense-report', function(req, res, next) {
 
 router.post('/expense-report', function(req, res, next){
 	var report = new Report(req.body);
-	report.save(function(err, report){
-    if(err){ return next(err); }
+    User.findOne({"name" : req.user}, "_id", function(err, id) {
+        if (err) {
+            return next(err);
+        }
+        report.user = id;
 
-		res.json(report);
-	});
+        report.save(function(err, report){
+        if(err){ return next(err); }
+
+            res.json(report);
+        });
+    });
 });
-
 
 // Get all line item types
 router.get('/line-item-types', function(req, res, next) {
     var lineItemTypes = [{name: 'Mileage'}, {name: 'Per Diem'}, {name: 'Lodging'}, {name: 'Travel'}, {name: 'Meals'}, {name: 'Entertainment'}, {name: 'Parking'}, {name: 'Other'}];
     res.json(lineItemTypes);
-});
-
-//router.get('/reports', function(req, res, next) {
-// router.get('/projects', function(req, res, next) {
-//    Project.find(function(err, projects) {
-//        if (err) {
-//            return next(err);
-//        }
-//        res.json(projects);
-//    });
-// });
-//
-// router.post('/reports', function(req, res, next) {
-//     var aReport = new Report(req.body);
-//     aReport.save(function(err, aReport) {
-//        if (err) {
-//            return next(err);
-//        }
-//        res.json(aReport);
-//    });
-//});
-
-router.get('/expense-report', function (req, res, next) {
-    Report.find(function(err, reports) {
-        if (err) {
-            return next(err);
-        }
-        res.json(reports);
-    });
-});
-router.post('/expense-report', function (req, res, next) {
-    console.log("attempting to post");
-    var expenseReport = new Report(req.body);
-
-    expenseReport.save(function(err, post){
-        if(err) { return next(err); }
-
-        res.json(post);
-    });
 });
 
 module.exports = router;
