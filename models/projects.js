f2d057d8c4143ad5dd04131349a0fe76314c82cd
Derek Delaney <ddelaney@catalysitservices.com>
2015-12-18 09:55:41 -0800

    removed commits

--- models/projects.js
+++ models/projects.js
@@ -1,8 +1,11 @@
 var mongoose = require('mongoose');
-//require('./users');
+
 var ProjectSchema = new mongoose.Schema({
     name: String,
-    approver: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}
+    approver: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
 });
 
 mongoose.model('Project', ProjectSchema);
--- public/js/controllers/userCtrl.js
+++ public/js/controllers/userCtrl.js
@@ -1,37 +1,34 @@
-
 app.controller('userCtrl', ['$scope', '$state', 'userFactory', 'sharedProperties',
-   function($scope, $state, userFactory, sharedProperties){
-	    $scope.user = {};
-		
-		$scope.setUsername = function(){
-			userFactory.getCurrentUser().then(function(success){
-				$scope.userName = success.data.name;
-				$scope.userId = success.data._id;
-				sharedProperties.setUserId($scope.userId);
-				
-			});
-			/*$scope.user = {
-							"_id": "56707a9e2c29fc36bf61955f",
-							"name": "dsloane"
-						};
-			$scope.userId = $scope.user._id;
-			$scope.userObject = {
-				"_id": $scope.userId
-			};*/
-		};
-		
-		$scope.clearData = function(){
-			sharedProperties.setExpenseReport({items:[]});
-		};
-		
-		$scope.newProject = function(){
-			$scope.clearData();
-			$state.go("createProject", {}, {reload: true});
-		};
-		
-		$scope.newExpenseReport = function(){
-			$scope.clearData();
-			$state.go("expenseReport", {}, {reload: true});
-		};
-	}                               
+   function ($scope, $state, userFactory, sharedProperties) {
+        $scope.user = {};
+
+        $scope.setUsername = function () {
+            userFactory.getCurrentUser().then(function (success) {
+                $scope.userName = success.data.name;
+                $scope.userId = success.data._id;
+                sharedProperties.setUserId($scope.userId);
+
+            });
+        };
+
+        $scope.clearData = function () {
+            sharedProperties.setExpenseReport({
+                items: []
+            });
+        };
+
+        $scope.newProject = function () {
+            $scope.clearData();
+            $state.go("createProject", {}, {
+                reload: true
+            });
+        };
+
+        $scope.newExpenseReport = function () {
+            $scope.clearData();
+            $state.go("expenseReport", {}, {
+                reload: true
+            });
+        };
+	}
 ]);
