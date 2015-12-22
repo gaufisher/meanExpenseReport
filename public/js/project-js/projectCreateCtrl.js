9ff002e845bab6476a3525053cfc2ac2a48583a4
Gavin Stringfellow <gstringfellow@catalystitservices.com>
2015-12-22 14:25:26 +0000

    toast appears red with

--- public/js/project-js/projectCreateCtrl.js
+++ public/js/project-js/projectCreateCtrl.js
@@ -31,7 +31,7 @@ angular.module('QuickrBooks').controller('projectCreateCtrl',['$scope', '$state'
     }
 
     $scope.cancel = function () {
-      toaster.pop('failure',"Canceled",`Creating project ${$scope.projectName}` )
+      toaster.pop('error',"Canceled",`Creating project ${$scope.projectName}` )
         $state.go("viewReports", {}, {
             reload: true
         });
