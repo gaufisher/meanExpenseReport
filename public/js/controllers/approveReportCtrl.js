7d52974386367c68df35a8ffba70c3fdc5bdce68
jjacobson <jjacobson@catalystitservices.com>
2015-12-24 09:52:07 -0800

    Added ability to approve/deny a report

--- public/js/controllers/approveReportCtrl.js
+++ public/js/controllers/approveReportCtrl.js
@@ -1,11 +1,20 @@
 'use strict';
 
-angular.module('QuickrBooks').controller('approveReportCtrl', ['$scope', '$state', 'Report',
-    function($scope, $state, Report) {
+angular.module('QuickrBooks').controller('approveReportCtrl', ['$scope', '$state', 'Report', 'expenseReportFactory',
+    function($scope, $state, Report, expenseReportFactory) {
         $scope.report = Report;
         console.log(Report);
         $scope.submit = function(status) {
-            //post it
+            console.log('Clicked');
+            $scope.report.status = status;
+            expenseReportFactory.updateExpenseReport($scope.report).then(
+                function(success) {
+                    $state.go('approveReports', {}, {reload: true});
+                },
+                function(error) {
+                    alert('NO!');
+                }
+            )
         }
     }
 ]);
--- routes/index.js
+++ routes/index.js
@@ -128,14 +128,27 @@ router.put('/expense-report', function(req, res, next){
 	if(rep.hasOwnProperty('items')){
 		for(var i = 0; i < rep.items.length; i++)
 		{
-			rep.items[i].value = rep.items[i].value * 100;
+			rep.items[i].value = rep.items[i].value.toString();
 		}
 	}
 	Report.findById(rep._id, function(err, report){
 		if(err){ return next(err);}
-        if ((report.status === "approved" || report.status === "denied") || (report.status !== "saved" && rep.status !== "saved")) {
-            //Will need to validate if approver is approving/denying
+        console.log(report);
+        console.log(rep);
+        if (rep.status === "approved" || rep.status === "denied") {
+            var anErr = false;
+            Project.findById(report.project, function(err, project) {
+                if (!project.approver.equals(req.user._id)) {
+                    req.status(500).json({error: 'You are not the approver for this report.'});
+                    anErr = true;
+                }
+                if (anErr) {
+                    return;
+                }
+            });
+        } else if ((report.status !== "saved" && rep.status !== "saved")) {
             console.log("I can't do that");
+            res.status(500).json({error: 'Cannot edit a report that doesn\'t have a saved status.'})
             return;
         }
 		for(var field in Report.schema.paths){
