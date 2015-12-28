233a482211eabddc0d6c8e16cc70dccfa197ccc0
Derek Delaney <ddelaney@catalysitservices.com>
2015-12-28 11:15:02 -0800

    removed comments

--- public/js/controllers/viewReportsCtrl.js
+++ public/js/controllers/viewReportsCtrl.js
@@ -2,36 +2,9 @@ app.controller('viewReportsCtrl', ['$scope', '$state', "expenseReportFactory", "
     function ($scope, $state, expenseReportFactory, projectFactory, ExpenseReports, sharedProperties) {
         $scope.reports = ExpenseReports.data;
         $scope.showReport = function (id) {
-//            expenseReportFactory.getById(report._id).then(
-//                function (success) {
-//                    var report = success.data;
-//                    if (report.hasOwnProperty('items')) {
-//                        for (var i = 0; i < report.items.length; i++) {
-//                            report.items[i].value = report.items[i].value / 100;
-//                        }
-//                    }
-//                    projectFactory.getAll().then(
-//                        function (success) {
-//                            var projects = success.data;
-//                            var projectId = 0;
-//                            for (var i = 0; i < projects.length; i++) {
-//                                if (projects[i]._id == report.project) {
-//                                    sharedProperties.setProjectId(i);
-//                                }
-//                            }
-//                        }
-//                    );
-//                    sharedProperties.setExpenseReport(report);
-                    console.log(id);
                     $state.go("viewReport", {id: id}, {
                         reload: true
                     });
                 };
-//                function (error) {}
-//            );
-
-        //};
-
-
     }
 ]);
