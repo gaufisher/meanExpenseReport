ebdb6e3c7f5807e19ad6eb3da5e444556fd92327
Dan Sloane <dsloane@catalystdevworks.com>
2015-12-28 10:02:16 -0800

    email on approve/reject working

--- routes/expense-report.js
+++ routes/expense-report.js
@@ -15,16 +15,15 @@ router.post('/', function(req, res, next) {
 	var transporter = nodemailer.createTransport("direct", {debug: true});
 	
 	var report = req.body;
-			
-	var subjectLine = "expense report status change";
 
 	var emailText = "<html><style>td, th{margin-right: 2em;} button{border: thin solid black; border-radius: 5px;} .rejection{color: red;}</style><h2>Name: <b>" + report.name + "</b></h2><h2>Line Items:</h2><table><thead><tr><th> Type </th><th> Amount </th></tr></thead><tbody>";
-	for(var i = 0; i < report.items.length; i++){
-		emailText += "<tr><td> " + report.items[i].type + " </td><td> $" + report.items[i].value + " </td></tr>";
-	}
-	emailText += "</tbody></table><h2>Status: " + report.status.toUpperCase() + "</h2>";
 	
 	if(report.status === "submitted" && report.user == req.user._id){ //back-end security
+		var subjectLine = "expense report submitted";
+		for(var i = 0; i < report.items.length; i++){
+			emailText += "<tr><td> " + report.items[i].type + " </td><td> $" + report.items[i].value + " </td></tr>";
+		}
+		emailText += "</tbody></table><h2>Status: " + report.status.toUpperCase() + "</h2>";
 		var userEmail = req.user.name + "@catalystitservices.com";
 		console.log("userEmail = " + userEmail);
 		
@@ -37,33 +36,6 @@ router.post('/', function(req, res, next) {
 			subject: subjectLine,
 			html: emailUserText
 		});
-		
-		Project.findById(report.project, function(err, project){
-			if (err) {
-				return next(err);
-			}
-			User.findById(project.approver, function(error, approver){
-				if(error){
-					return next(error);
-				}
-				var approverEmail = approver.name + "@catalystitservices.com";
-				var emailApproverText = "The following report has been " + report.status + " for your approval.<br>" + emailText;
-			
-				emailApproverText += "<h2>Project: " + project.name + "</h2><h2>Submitted By: " + req.user.name + "</h2>";
-				if(report.hasOwnProperty('notes')){
-					emailApproverText += "<h2>Notes: " + report.notes + "</h2></html>";
-				}
-				emailApproverText += "<a href='http://localhost:8080/app#/expense-report/" + report._id + "'><button>View Report</button></a></html>";
-				
-				transporter.sendMail({
-					from: 'donotreply@quickrbooks.com',
-					to: approverEmail,
-					subject: subjectLine,
-					html: emailApproverText
-				});
-			});
-					
-		});		
 	}
 	if(report.status === "approved" || report.status === "denied")
 	{
@@ -75,11 +47,19 @@ router.post('/', function(req, res, next) {
 				if (error) {
 					return next(err);
 				}
+				var subjectLine = "expense report " + report.status;
+				for(var i = 0; i < report.items.length; i++){
+					emailText += "<tr><td> " + report.items[i].type + " </td><td> $" + (report.items[i].value)/100 + " </td></tr>";
+				}
+				emailText += "</tbody></table><h2>Status: " + report.status.toUpperCase() + "</h2>";
+				
 				var userEmail = user.name + "@catalystitservices.com";
 				var emailUserText = "The following report has been " + report.status + ".<br>" + emailText;
 				emailUserText += "<h2>Project: " + project.name + "</h2>";
-				if(report.status === "denied" && report.hasOwnProperty('rejectionReason')){
-					emailUserText += "<h2>Reason for Rejection: <span class='rejection'" + report.rejectionReason + "</span></h2>";
+				if(report.status === "denied" && report.hasOwnProperty('rejections')){
+					for(var i = 0; i < report.rejections.length; i++){
+						emailUserText += "<h2>Reason for Rejection: <span class='rejection'" + report.rejection[i].reason + "</span></h2>";
+					}
 				}
 				emailUserText += "</html>";
 			
