f17192f625e80bb94df40c9123ca517bd9ac721d
Derek Delaney <ddelaney@catalysitservices.com>
2015-12-28 11:10:52 -0800

    added comma

--- models/reports.js
+++ models/reports.js
@@ -26,7 +26,7 @@ var ReportSchema = new mongoose.Schema({
                 }
             },
     notes: String,
-    unsubmitReasons: [{date:{type: Date, default: Date.now},notes:String}]
+    unsubmitReasons: [{date:{type: Date, default: Date.now},notes:String}],
     rejections: [
         {
             reason: {
