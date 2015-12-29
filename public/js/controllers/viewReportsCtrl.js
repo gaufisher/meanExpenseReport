35b20d10cc2e02b3eb4666e5f36d4c67f75ed6d1
Greg Fisher <gfisher@catalystitservices.com>
2015-12-29 08:46:19 -0800

    added some styling to the view all reports page

--- public/js/controllers/viewReportsCtrl.js
+++ public/js/controllers/viewReportsCtrl.js
@@ -4,15 +4,14 @@ app.controller('viewReportsCtrl', ['$scope', '$state', "expenseReportFactory", "
         $scope.reports = ExpenseReports.data;
         $scope.reportsByApprover = ExpenseReportsByApprover;
         $scope.statuses = [
-            {name: 'saved'},
-            {name: 'submitted'},
-            {name: 'approved'},
-            {name: 'denied'}
+            {name: 'Saved'},
+            {name: 'Submitted'},
+            {name: 'Approved'}
         ];
 
         $scope.approverLinks = [
-            {name: 'pending my approval', status: 'submitted'},
-            {name: 'already approved', status: 'approved'}
+            {name: 'Pending my Approval', status: 'submitted'},
+            {name: 'Already Approved', status: 'approved'}
         ];
   
         $scope.showReport = function (id) {
