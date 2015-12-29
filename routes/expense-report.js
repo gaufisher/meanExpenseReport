6dea1af40f75313ebbb5917be12035633928c360
Dan Sloane <dsloane@catalystdevworks.com>
2015-12-29 13:59:31 -0800

    fixed link in email to have actual report id

--- routes/expense-report.js
+++ routes/expense-report.js
@@ -16,78 +16,84 @@ router.post('/', function(req, res, next) {
 	var report = req.body;
 			
 	var subjectLine = "expense report status change";
-
+	
 	if(report.status === "submitted"){ 
-		var userEmail = req.user.name + "@catalystitservices.com";
-		console.log("userEmail = " + userEmail);
-		var emailText = "<html><style>td, th{margin-right: 2em;} button{border: thin solid black; border-radius: 5px;}</style><h2>Name: <b>" + report.name + "</b></h2><h2>Line Items:</h2><table><thead><tr><th> Type </th><th> Amount </th></tr></thead><tbody>";
-		for(var i = 0; i < report.items.length; i++){
-			emailText += "<tr><td> " + report.items[i].type + " </td><td> $" + report.items[i].value + " </td></tr>";
-		}
-		emailText += "</tbody></table><h2>Status: " + report.status.toUpperCase() + "</h2>";
-		
-		var emailUserText = "The following report has been " + report.status + ".<br>" + emailText + "</html>";
-		
-		transporter.sendMail({
-			from: 'donotreply@quickrbooks.com',
-			to: userEmail,
-			subject: subjectLine,
-			html: emailUserText
-		});
-		
-		Project.findById(report.project, function(err, project){
+		Report.findOne({"name": report.name}, function(err, rep) {
 			if (err) {
-				return next(err);
+				return res.status(500).json(err);
 			}
-			User.findById(project.approver, function(error, approver){
-				if(error){
-					return next(error);
-				}
-				var approverEmail = approver.name + "@catalystitservices.com";
-				var emailApproverText = "The following report has been " + report.status + " for your approval.<br>" + emailText;
-				var attachments = function(){
-					var attachments = [];
-					for(var i = 0; i < report.receipts.length; i++){
-						attachments.push({});
-						attachments[i].filename = report.receipts[i].name;
-						attachments[i].contents = new Buffer(report.receipts[i].dataString, 'base64');
-						attachments[i].cid = report.receipts[i]._id;
-					}
-					console.log("attachments = ");
-					console.log(attachments);
-					
-					return attachments;
-				};
-				emailApproverText += "<h2>Project: " + project.name + "</h2><h2>Submitted By: " + req.user.name + "</h2>";
-				if(report.hasOwnProperty('notes')){
-					emailApproverText += "<h2>Notes: " + report.notes + "</h2>";
-				}
-				if(report.hasOwnProperty('receipts') && report.receipts.length > 0){
-					emailApproverText += "<h2>Receipts: See attached files</h2>"
-				}
-				emailApproverText += "<a href='http://localhost:8080/app#/approveReport/" + report._id + "'><button>View Report</button></a></html>";
-				
-				if(report.hasOwnProperty('receipts') && report.receipts.length > 0){
-					transporter.sendMail({
-						from: 'donotreply@quickrbooks.com',
-						to: approverEmail,
-						subject: subjectLine,
-						html: emailApproverText,
-						attachments: attachments()
-					});
-				}
-				else{
-					transporter.sendMail({
-						from: 'donotreply@quickrbooks.com',
-						to: approverEmail,
-						subject: subjectLine,
-						html: emailApproverText
-					});
-				}
-				
+			var userEmail = req.user.name + "@catalystitservices.com";
+			console.log("userEmail = " + userEmail);
+			var emailText = "<html><style>td, th{margin-right: 2em;} button{border: thin solid black; border-radius: 5px;}</style><h2>Name: <b>" + report.name + "</b></h2><h2>Line Items:</h2><table><thead><tr><th> Type </th><th> Amount </th></tr></thead><tbody>";
+			for(var i = 0; i < report.items.length; i++){
+				emailText += "<tr><td> " + report.items[i].type + " </td><td> $" + report.items[i].value + " </td></tr>";
+			}
+			emailText += "</tbody></table><h2>Status: " + report.status.toUpperCase() + "</h2>";
+			
+			var emailUserText = "The following report has been " + report.status + ".<br>" + emailText + "</html>";
+			
+			transporter.sendMail({
+				from: 'donotreply@quickrbooks.com',
+				to: userEmail,
+				subject: subjectLine,
+				html: emailUserText
 			});
+			
+			Project.findById(report.project, function(err, project){
+				if (err) {
+					return next(err);
+				}
+				User.findById(project.approver, function(error, approver){
+					if(error){
+						return next(error);
+					}
+					var approverEmail = approver.name + "@catalystitservices.com";
+					var emailApproverText = "The following report has been " + report.status + " for your approval.<br>" + emailText;
+					var attachments = function(){
+						var attachments = [];
+						for(var i = 0; i < report.receipts.length; i++){
+							attachments.push({});
+							attachments[i].filename = report.receipts[i].name;
+							attachments[i].contents = new Buffer(report.receipts[i].dataString, 'base64');
+							attachments[i].cid = report.receipts[i]._id;
+						}
+						console.log("attachments = ");
+						console.log(attachments);
+						
+						return attachments;
+					};
+					emailApproverText += "<h2>Project: " + project.name + "</h2><h2>Submitted By: " + req.user.name + "</h2>";
+					if(report.hasOwnProperty('notes')){
+						emailApproverText += "<h2>Notes: " + report.notes + "</h2>";
+					}
+					if(report.hasOwnProperty('receipts') && report.receipts.length > 0){
+						emailApproverText += "<h2>Receipts: See attached files</h2>"
+					}
+					emailApproverText += "<a href='http://localhost:8080/app#/approveReport/" + rep._id + "'><button>View Report</button></a></html>";
 					
+					if(report.hasOwnProperty('receipts') && report.receipts.length > 0){
+						transporter.sendMail({
+							from: 'donotreply@quickrbooks.com',
+							to: approverEmail,
+							subject: subjectLine,
+							html: emailApproverText,
+							attachments: attachments()
+						});
+					}
+					else{
+						transporter.sendMail({
+							from: 'donotreply@quickrbooks.com',
+							to: approverEmail,
+							subject: subjectLine,
+							html: emailApproverText
+						});
+					}
+					
+				});
+						
+			});
 		});
+		
 		
 	}
 	
